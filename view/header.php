bb6e5b532419289b5cbdaedf9946a8c4803151af
Bruno Vogel <bugvogel@gmail.com>
2018-04-17 10:17:43 -0300

    architecture folders

---
+++ view/header.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+    <a class="navbar-brand" href="#"><img id="logo" src="https://trello-attachments.s3.amazonaws.com/5abc0840da1c11f6c0bbdc57/5ad0b9f7a00f3dd3d33eb46c/f6adda0082c8ece2c9673b88dd274093/logo-wheremelon.png"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="input-group mb-3 col-7 mx-auto">
+        <input type="text" class="form-control form-control-lg rounded-left" placeholder="O que você deseja buscar" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            
+        <button class="btn btn-light transparent" type="button"><i class="fas fa-search"></i></button>
+            
+      </div>
+      <span class="oi oi-cart"></span>
+      
+      <ul class="navbar-nav ml-auto">
+        <!--<a href="#"><i class="far fa-user-circle"></i></a>-->
+        <li class="nav-item dropdown mr-2">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i id="usericon" class="far fa-user-circle fa-2x"></i>
+            Minha Conta
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Login</a>
+            <a class="dropdown-item" href="#">Registrar-se</a>
+          </div>
+        </li>
+        <a class="nav-link" href="#"><i id="cart"class="fas fa-shopping-cart fa-2x"></i></a>
+        <!--<a href="#"><i id="cart"class="fas fa-shopping-cart"></i></a>-->
+      </ul>
+      
+    </div>
+  </nav>
